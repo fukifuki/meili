95a2dfb5393e0aca300dc8d011e89c24f0b05806
Rade Krstanovic <rade.krstanovic@endava.com>
2020-07-27 13:54:37 +0200

    Create connection to MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 // route files
 const guides = require('./routes/guides');
 
@@ -7,10 +8,13 @@ dotenv.config({path: './config/.env'});
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
 // mount routers
 app.use('/api/v1/guides', guides);
+// connect to database
+connectDB();
 
-app.listen(PORT, () => {
+app.listen(
+    PORT,
+    () => {
     console.log(`Meili server is running in ${process.env.NODE_ENV} on port ${PORT}`)
 });
